410c022875f1db71731c6aa2cf68074a0ca9b801
Josué Moreau <jsmr14.12@gmail.com>
2025-09-07 15:51:57 +0200

    try 4

--- src/main/kotlin/org/bsplines/ltexls/parsing/typst/TypstAnnotatedTextBuilder.kt
+++ src/main/kotlin/org/bsplines/ltexls/parsing/typst/TypstAnnotatedTextBuilder.kt
@@ -18,6 +18,7 @@ class TypstAnnotatedTextBuilder(
   private var mathModeStringCounter = 0
 
   override fun processCharacter() {
+    addMarkup(Regex("\\\\"), "", true)
     processEscapeCharacter()
     addMarkup(NO_TEXT_INLINE_MATH_REGEX, "", true)
     processMathBlock()
